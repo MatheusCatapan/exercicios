c6d80e313f97946115cc658960de5e312faf9874
MatheusCatapan <m.oliveira.mo355@gmail.com>
2025-05-19 08:25:50 -0300

    2d done

--- exercicio2d.php
+++ exercicio2d.php
@@ -6,8 +6,11 @@
     
     //Valor em reais de cada quilowatt
     $kw = ($salario / 7) / 100;
-    $kw = $kw * $quilowatts;
-
+    echo "R$" . number_format($kw, 2) . "\n";
     //Valor em reais a ser pago
     $vTotal = $kw * $quilowatts;
-    echo $vTotal;
+    echo "R$" . number_format($vTotal, 2) . "\n";
+
+    //Valor a ser pago com desconto
+    $vDesconto = $vTotal * 0.9 . "\n";
+    echo "R$" . number_format($vDesconto, 2) . "\n";
